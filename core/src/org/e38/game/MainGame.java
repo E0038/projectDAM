ede155819fb409ad0d6b6daaeddffb57f96dad97
sergi <sergi.e0038@gmail.com>
2016-04-28 15:26:54 +0200

    Screen dispose on change

--- core/src/org/e38/game/MainGame.java
+++ core/src/org/e38/game/MainGame.java
@@ -2,16 +2,16 @@ package org.e38.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import org.e38.game.utils.AssertLoader;
 import org.e38.game.screens.SplashScreen;
+import org.e38.game.utils.AssertLoader;
 
 public class MainGame extends Game {
     SpriteBatch batch;
     Texture img;
-
 
     @Override
     public void create() {
@@ -33,5 +33,20 @@ public class MainGame extends Game {
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
+    }
+
+    @Override
+    public void setScreen(Screen screen) {
+        synchronized (this) {
+            if (this.screen != null) {
+                this.screen.hide();
+                this.screen.dispose();
+            }
+            this.screen = screen;
+            if (this.screen != null) {
+                this.screen.show();
+                this.screen.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            }
+        }
     }
 }
