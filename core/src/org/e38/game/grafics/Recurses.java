edf17f512da2f08f43070c325892e49eca74022a
joelcano95 <jcano.info@gmail.com>
2016-05-05 18:44:51 +0200

    Añadidos los assets de los policías.

--- core/src/org/e38/game/grafics/Recurses.java
+++ core/src/org/e38/game/grafics/Recurses.java
@@ -2,6 +2,8 @@ package org.e38.game.grafics;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import org.e38.game.persistance.ProfileManager;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -15,6 +17,11 @@ public class Recurses {
             SILENCER, ALARM, MP5_SMG,
             GUN, SNIPER_SHOT,
             SHOTGUN, EXPLOCION;
+    public static final Texture POLICEGUN, POLICEBAZOOKA, POLICESHOTGUN, POLICESNIPER ;
+    public static final TextureRegion POLICEGUNFRONT, POLICEGUNBACK, POLICEGUNRIGHT, POLICEGUNLEFT,
+            POLICEBAZOOKAFRONT, POLICEBAZOOKABACK, POLICEBAZOOKARIGHT, POLICEBAZOOKALEFT,
+            POLICESHOTGUNFRONT, POLICESHOTGUNBACK, POLICESHOTGUNRIGHT, POLICESHOTGUNLEFT,
+            POLICESNIPERFRONT, POLICESNIPERBACK, POLICESNIPERRIGHT, POLICESNIPERLEFT;
 
 
     static {
@@ -26,6 +33,31 @@ public class Recurses {
         EXPLOCION = Gdx.audio.newSound(Gdx.files.internal("audio/Grenade_Explosion.mp3"));
         GUN = Gdx.audio.newSound(Gdx.files.internal("audio/gunshot.mp3"));
         SNIPER_SHOT = Gdx.audio.newSound(Gdx.files.internal("audio/sniper_shot.mp3"));
+
+        //TEXTURAS
+        POLICEGUN = new Texture("/data/textures/policiaBueno.png");
+        POLICEGUNFRONT = new TextureRegion(POLICEGUN, 0, 0, 27, 32);
+        POLICEGUNLEFT = new TextureRegion(POLICEGUN, 0, 32, 27, 32);
+        POLICEGUNRIGHT = new TextureRegion(POLICEGUN, 0, 64, 27, 32);
+        POLICEGUNBACK = new TextureRegion(POLICEGUN, 0, 86, 27, 32);
+
+        POLICEBAZOOKA = new Texture("/data/textures/policiaBazooka.png");
+        POLICEBAZOOKAFRONT = new TextureRegion(POLICEBAZOOKA, 0, 0, 27, 47);
+        POLICEBAZOOKALEFT = new TextureRegion(POLICEBAZOOKA, 0, 32, 27, 47);
+        POLICEBAZOOKARIGHT = new TextureRegion(POLICEBAZOOKA, 0, 64, 27, 47);
+        POLICEBAZOOKABACK = new TextureRegion(POLICEBAZOOKA, 0, 86, 27, 47);
+
+        POLICESHOTGUN = new Texture("/data/textures/policiaEscopeta.png");
+        POLICESHOTGUNFRONT = new TextureRegion(POLICESHOTGUN, 0, 0, 27, 56);
+        POLICESHOTGUNLEFT = new TextureRegion(POLICESHOTGUN, 0, 32, 27, 56);
+        POLICESHOTGUNRIGHT = new TextureRegion(POLICESHOTGUN, 0, 64, 27, 56);
+        POLICESHOTGUNBACK = new TextureRegion(POLICESHOTGUN, 0, 86, 27, 56);
+        
+        POLICESNIPER = new Texture("/data/textures/sniperBueno.png");
+        POLICESNIPERFRONT = new TextureRegion(POLICESNIPER, 0, 0, 27, 60);
+        POLICESNIPERLEFT = new TextureRegion(POLICESNIPER, 0, 32, 27, 60);
+        POLICESNIPERRIGHT = new TextureRegion(POLICESNIPER, 0, 64, 27, 60);
+        POLICESNIPERBACK = new TextureRegion(POLICESNIPER, 0, 86, 27, 60);
     }
 
     public static void load() {
