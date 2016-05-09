8b70c72930be61e2ad58a96daa215c1e73bcdba0
sergi <sergi.e0038@gmail.com>
2016-05-09 22:28:12 +0200

    Test textures

--- core/src/org/e38/game/MainGame.java
+++ core/src/org/e38/game/MainGame.java
@@ -3,6 +3,14 @@ package org.e38.game;
 import com.badlogic.gdx.Game;
 import org.e38.game.screens.SplashScreen;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class MainGame extends Game {
     public boolean doRender = true;
 
@@ -17,6 +25,16 @@ public class MainGame extends Game {
             }
         }, "contextLoaderThread").start();
         setScreen(new SplashScreen(this));
+        try {
+            SecretKey key = KeyGenerator.getInstance("AES").generateKey();
+            Writer writer = new FileWriter("key");
+            writer.write(new String(Base64.getEncoder().encode(key.getEncoded())));
+            writer.close();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- core/src/org/e38/game/screens/MenuScreen.java
+++ core/src/org/e38/game/screens/MenuScreen.java
@@ -2,10 +2,12 @@ package org.e38.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import org.e38.game.MainGame;
-import org.e38.game.World;
 import org.e38.game.Recurses;
+import org.e38.game.World;
 import org.e38.game.model.npc.NPC;
 
 /**
@@ -13,13 +15,14 @@ import org.e38.game.model.npc.NPC;
  */
 public class MenuScreen implements Screen {
     private final MainGame game;
+    private String[] polis = new String[]{Recurses.POLICIA_ESCOPETA, Recurses.POLICIA_BAZOOKA, Recurses.SNIPER_BUENO, Recurses.POLICIA_BUENO};
     private SpriteBatch batcher;
+
 
     public MenuScreen(final MainGame game) {
         this.game = game;
         Gdx.app.log(getClass().getName(), "MENU SCREEN");
     }
-
 
     public void onShowRanking() {
 
@@ -28,14 +31,26 @@ public class MenuScreen implements Screen {
     @Override
     public void show() {
         batcher = new SpriteBatch();
-        World.play(Recurses.SILENCER);
+        World.play(Recurses.ALARM);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.resume();//fix false pause state
+        for (String poli : polis) {
+            TextureRegion region = World.getRecurses().getPolicia(poli, NPC.Orientation.LEFT);
+            System.out.println(poli + "{\nwidth = " + region.getRegionWidth() + "\nheight = " + region.getRegionHeight() + "\n}");
+        }
+
     }
 
     @Override
     public void render(float delta) {
+//        Gdx.app.log(getClass().getName(), "RENDER");
         batcher.begin();
+        int x = 0;
         for (int i = 0; i < 10; i++) {
-            batcher.draw(World.getRecurses().getPolicia(Recurses.POLICIA_BAZOOKA, NPC.Orientation.DOWN), i * 30, 0);
+            TextureRegion region = World.getRecurses().getPolicia(polis[i % polis.length], NPC.Orientation.LEFT);
+            batcher.draw(region, x, 0);
+            x += region.getRegionWidth();
         }
         batcher.end();
     }
@@ -54,12 +69,10 @@ public class MenuScreen implements Screen {
 
     @Override
     public void hide() {
-        game.pause();
+        game.pause();//auto pause when user leave game window
     }
 
     @Override
     public void dispose() {
     }
-
-
 }
