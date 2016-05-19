bc1d7a8a839bea714a83537dfd44189dab5b8842
sergi <sergi.e0038@gmail.com>
2016-05-19 17:22:20 +0200

    pruevas screen clicks

--- core/src/org/e38/game/InputHandler.java
+++ core/src/org/e38/game/InputHandler.java
@@ -1,7 +1,8 @@
 package org.e38.game;
 
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapProperties;
 import org.e38.game.screens.LevelScreen;
 
 import java.util.ArrayList;
@@ -11,11 +12,57 @@ import java.util.List;
 /**
  * Created by joel on 18/05/2016.
  */
-public class InputHandler implements InputProcessor{
+public class InputHandler implements InputProcessor {
     LevelScreen lvl;
+    private MapObject[][] mapObjects = new MapObject[100][100];
 
     public InputHandler(LevelScreen lvl) {
         this.lvl = lvl;
+        int c = 0;
+        List<MapObject> objects = iterToList(lvl.getLevel().getLayer().getObjects().iterator());
+        for (MapObject object : objects) {
+            if (object.getProperties().get("type") != null && !object.getProperties().get("type").equals("camino")) {
+                System.out.println(propetresToString(object.getProperties()));
+            }
+        }
+        System.out.println(objects);
+        for (MapObject object : objects) {
+            int x, y;
+            x = (int) ((Float) object.getProperties().get("x") / 8);
+            y = (int) ((Float) object.getProperties().get("y") / 6);
+            float width = (Float) object.getProperties().get("width");
+            float height = (Float) object.getProperties().get("height");
+
+            mapObjects[x][y] = object;
+            int xCasillas = Math.round(width / 8);
+            int yCasillas = Math.round(height / 6);
+            for (int i = x - xCasillas / 2; i < x + xCasillas / 2; i++) {
+                for (int j = y - yCasillas / 2; j < y + yCasillas / 2; j++) {
+                    mapObjects[i][j] = object;
+                }
+            }
+        }
+        lvl.getLevel().getLayer().getObjects().iterator();
+    }
+
+    private <T> List<T> iterToList(Iterator<T> tIterator) {
+        List<T> list = new ArrayList<>();
+        while (tIterator.hasNext()) {
+            list.add(tIterator.next());
+        }
+        return list;
+    }
+
+    private String propetresToString(MapProperties properties) {
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iterator = properties.getKeys();
+        builder.append("{\n");
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            builder.append("\t" + key + " : " + properties.get(key) + ",\n");
+        }
+        builder.append("}");
+        return builder.toString();
     }
 
     @Override
@@ -32,26 +79,15 @@ public class InputHandler implements InputProcessor{
     public boolean keyTyped(char character) {
         return false;
     }
-private  static List iterToList(Iterator tIterator){
-    List list = new ArrayList<>();
-    while (tIterator.hasNext()){
-        list.add(tIterator.next());
-    }
-        return list;
-}
+
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-//        System.out.println("height = " + lvl.getLevel().getLayer().getTileHeight());
         try {
-            //lvl.getLevel().getMap().getLayers();
-            System.out.println(iterToList(lvl.getLevel().getLayer().getObjects().iterator().next().getProperties().getKeys()));
-//            TiledMapTileLayer.Cell cell = lvl.getLevel().getLayer().getCell(screenX/8, screenY/6);
-//            java.util.Iterator<java.lang.Object> iterator =cell.getTile().getProperties().getValues();
-
-//            System.out.println(iterToList(lvl.getLevel().getMap().getTileSets().iterator()));
-//            System.out.println(cell.getTile().getTextureRegion());
-//            System.out.println(iterToList(cell.getTile().getProperties().getKeys()).toString());
-        }catch(Exception e){
+//            screenY = 600 - screenY;//normalizar y
+            System.out.println(screenX + " : " + screenY);
+            MapProperties properties = mapObjects[Math.round(screenX / 8)][Math.round(screenY / 6)].getProperties();
+            System.out.println(propetresToString(properties));
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         lvl.getCopsBar().updateBar(30);
