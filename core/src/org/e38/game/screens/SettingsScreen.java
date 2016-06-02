7f15cd555a7d155f20012a38edba58107f54f609
joelcano95 <jcano.info@gmail.com>
2016-06-02 16:33:25 +0200

    Añadido default.fnt -> -settings funciona en android

--- core/src/org/e38/game/screens/SettingsScreen.java
+++ core/src/org/e38/game/screens/SettingsScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
